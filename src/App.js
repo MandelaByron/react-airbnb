8ef12bc84921968ac1a8a224c21396e879647be9
Byron Mandela <mandelabyron@gmail.com>
2023-01-07 06:47:07 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+      img="https://images.unsplash.com/photo-1598623335306-5d0040e41f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXRobGV0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60agesc/"
+      ratings="5"
+      reviewCount={6}
+      country="USA"
+      title="No pain No gain. Zara's everyday workout routine."
+      price={138}
+      />
       
     </div>
   )
--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props)
     return(
     <div className="card">
-        <img src="https://images.unsplash.com/photo-1598623335306-5d0040e41f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXRobGV0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className="card--image"/>
+        <img src={`${props.img}`} className="card--image"/>
         <div className='card--stats'>
-            <span class="material-symbols-outlined">star</span>
-            <span>5.0</span>
-            <span>(6) .</span>
-            <span>USA</span>
+            <span className="material-symbols-outlined">star</span>
+            <span>{props.ratings} </span>
+            <span>({props.reviewCount})  .</span>
+            <span>{props.country}</span>
         </div>
-        <p>No pain No gain. Zara's everyday workout routine.</p>
-        <p><span className="bold">From $136</span> / person</p>
+        <p>{props.title}</p>
+        <p><span className="bold">From ${props.price}</span> / person</p>
 
             
         
