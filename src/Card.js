75c98b8e34905dad675a071ce7a603683706290a
Byron Mandela <mandelabyron@gmail.com>
2023-01-07 06:48:13 +0000

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,24 +1,20 @@
 import React from "react";
 
 export default function Card(props) {
-    console.log(props)
-    return(
+  console.log(props);
+  return (
     <div className="card">
-        <img src={`${props.img}`} className="card--image"/>
-        <div className='card--stats'>
-            <span className="material-symbols-outlined">star</span>
-            <span>{props.ratings} </span>
-            <span>({props.reviewCount})  .</span>
-            <span>{props.country}</span>
-        </div>
-        <p>{props.title}</p>
-        <p><span className="bold">From ${props.price}</span> / person</p>
-
-            
-        
+      <img src={`${props.img}`} className="card--image" />
+      <div className="card--stats">
+        <span className="material-symbols-outlined">star</span>
+        <span>{props.ratings} </span>
+        <span>({props.reviewCount}) .</span>
+        <span>{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
     </div>
-        
-    )
-
-     
+  );
 }
